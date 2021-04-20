cfe853e7bbbd7a1e1e753e7af31508d05689fb9c
Nick Eccher <nicholas.eccher@owen.alumni.vanderbilt.edu>
2021-04-20 13:43:59 -0500

    Deliverable 2 of Challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -28,3 +28,21 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+--Mentorship Eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no
+	, e.first_name
+	, e.last_name
+	, e.birth_date
+	, de. from_date
+	, de. to_date
+	, t.title
+INTO mentorship_eligibility
+FROM employees e
+INNER JOIN dept_emp de
+	ON e.emp_no = de.emp_no
+INNER JOIN titles t
+	ON e.emp_no = t.emp_no
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31' 
+ORDER BY e.emp_no, t.to_date DESC
